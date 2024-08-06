4345de3917b320a150b25a711d7690cf61761ed0
Paloma <paloflobarr05@gmail.com>
2024-08-06 11:18:06 -0400

    Add files via upload

---
+++ TAREA 10/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+      <TodosIssues />
+    </div>
+  </template>
+  
+  <script>
+  import TodosIssues from './components/TodosIssues.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      TodosIssues,
+    },
+  };
+  </script>
+  
+  <style>
+  /* Agrega estilos globales si es necesario */
+  </style>
