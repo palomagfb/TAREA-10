24ef682faed787ce7b1f7c535e53686fb2a6359d
Paloma <paloflobarr05@gmail.com>
2024-08-14 09:29:17 -0400

    Add files via upload

---
+++ TAREA 10/TAREA VUE.JS/src/components/TodoItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-col :span="12">
+      <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
+        <el-row v-if="todo" :gutter="12">
+          <el-col  :span="21"> {{todo}} </el-col>
+          <el-col :span="3">
+            <el-button @click="remove()" type="success" icon="el-icon-check" circle></el-button>
+          </el-col>
+        </el-row>
+        <el-row v-else :gutter="12">
+          <el-col  :span="21"> {{issue}} </el-col>
+          <el-col :span="3">
+            <el-button @click="remove()" type="success" icon="el-icon-check" circle></el-button>
+          </el-col>
+        </el-row>
+      </el-card>
+    </el-col>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TodoItem',
+  props: {
+    todo: {
+     type: String,
+     required: false
+    },
+    issue: {
+      type: String,
+      required: false
+    }
+  },
+  methods: {
+    remove() {
+      this.$emit('remove')
+    }
+  }
+}
+</script>
---
+++ TAREA 10/TAREA VUE.JS/src/components/__VLS_template.js
@@ -0,0 +1,120 @@
+import { __VLS_internalComponent, __VLS_componentsOption, __VLS_name } from './TodoItem.vue';
+
+function __VLS_template() {
+let __VLS_ctx!: InstanceType<__VLS_PickNotAny<typeof __VLS_internalComponent, new () => {}>> & {};
+/* Components */
+let __VLS_otherComponents!: NonNullable<typeof __VLS_internalComponent extends { components: infer C; } ? C : {}> & typeof __VLS_componentsOption;
+let __VLS_own!: __VLS_SelfComponent<typeof __VLS_name, typeof __VLS_internalComponent & (new () => { $scopedSlots: typeof __VLS_slots; })>;
+let __VLS_localComponents!: typeof __VLS_otherComponents & Omit<typeof __VLS_own, keyof typeof __VLS_otherComponents>;
+let __VLS_components!: typeof __VLS_localComponents & __VLS_GlobalComponents & typeof __VLS_ctx;
+/* Style Scoped */
+type __VLS_StyleScopedClasses = {};
+let __VLS_styleScopedClasses!: __VLS_StyleScopedClasses | keyof __VLS_StyleScopedClasses | (keyof __VLS_StyleScopedClasses)[];
+/* CSS variable injection */
+/* CSS variable injection end */
+let __VLS_resolvedLocalAndGlobalComponents!: {} &
+__VLS_WithComponent<'ElCol', typeof __VLS_localComponents, "ElCol", "elCol", "el-col"> &
+__VLS_WithComponent<'ElCard', typeof __VLS_localComponents, "ElCard", "elCard", "el-card"> &
+__VLS_WithComponent<'ElRow', typeof __VLS_localComponents, "ElRow", "elRow", "el-row"> &
+__VLS_WithComponent<'ElButton', typeof __VLS_localComponents, "ElButton", "elButton", "el-button">;
+__VLS_intrinsicElements.div; __VLS_intrinsicElements.div;
+__VLS_components.ElCol; __VLS_components.ElCol; __VLS_components.ElCol; __VLS_components.ElCol; __VLS_components.ElCol; __VLS_components.ElCol; __VLS_components.elCol; __VLS_components.elCol; __VLS_components.elCol; __VLS_components.elCol; __VLS_components.elCol; __VLS_components.elCol; __VLS_components["el-col"]; __VLS_components["el-col"]; __VLS_components["el-col"]; __VLS_components["el-col"]; __VLS_components["el-col"]; __VLS_components["el-col"];
+// @ts-ignore
+[ElCol, ElCol, ElCol, ElCol, ElCol, ElCol,];
+__VLS_components.ElCard; __VLS_components.ElCard; __VLS_components.elCard; __VLS_components.elCard; __VLS_components["el-card"]; __VLS_components["el-card"];
+// @ts-ignore
+[ElCard, ElCard,];
+__VLS_components.ElRow; __VLS_components.ElRow; __VLS_components.elRow; __VLS_components.elRow; __VLS_components["el-row"]; __VLS_components["el-row"];
+// @ts-ignore
+[ElRow, ElRow,];
+__VLS_components.ElButton; __VLS_components.ElButton; __VLS_components.elButton; __VLS_components.elButton; __VLS_components["el-button"]; __VLS_components["el-button"];
+// @ts-ignore
+[ElButton, ElButton,];
+{
+const __VLS_0 = __VLS_intrinsicElements["div"];
+const __VLS_1 = __VLS_elementAsFunctionalComponent(__VLS_0);
+const __VLS_2 = __VLS_1({ ...{}, }, ...__VLS_functionalComponentArgsRest(__VLS_1));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_0, typeof __VLS_2> & Record<string, unknown>) => void)({ ...{}, });
+const __VLS_3 = __VLS_pickFunctionalComponentCtx(__VLS_0, __VLS_2)!;
+let __VLS_4!: __VLS_NormalizeEmits<typeof __VLS_3.emit>;
+{
+const __VLS_5 = ({} as 'ElCol' extends keyof typeof __VLS_ctx ? { 'ElCol': typeof __VLS_ctx.ElCol; } : 'elCol' extends keyof typeof __VLS_ctx ? { 'ElCol': typeof __VLS_ctx.elCol; } : 'el-col' extends keyof typeof __VLS_ctx ? { 'ElCol': (typeof __VLS_ctx)["el-col"]; } : typeof __VLS_resolvedLocalAndGlobalComponents).ElCol;
+const __VLS_6 = __VLS_asFunctionalComponent(__VLS_5, new __VLS_5({ ...{}, span: ((12)), }));
+({} as { ElCol: typeof __VLS_5; }).ElCol;
+({} as { ElCol: typeof __VLS_5; }).ElCol;
+const __VLS_7 = __VLS_6({ ...{}, span: ((12)), }, ...__VLS_functionalComponentArgsRest(__VLS_6));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_5, typeof __VLS_7> & Record<string, unknown>) => void)({ ...{}, span: ((12)), });
+const __VLS_8 = __VLS_pickFunctionalComponentCtx(__VLS_5, __VLS_7)!;
+let __VLS_9!: __VLS_NormalizeEmits<typeof __VLS_8.emit>;
+{
+const __VLS_10 = ({} as 'ElCard' extends keyof typeof __VLS_ctx ? { 'ElCard': typeof __VLS_ctx.ElCard; } : 'elCard' extends keyof typeof __VLS_ctx ? { 'ElCard': typeof __VLS_ctx.elCard; } : 'el-card' extends keyof typeof __VLS_ctx ? { 'ElCard': (typeof __VLS_ctx)["el-card"]; } : typeof __VLS_resolvedLocalAndGlobalComponents).ElCard;
+const __VLS_11 = __VLS_asFunctionalComponent(__VLS_10, new __VLS_10({ ...{}, class: ("box-card"), shadow: ("hover"), style: ({}), }));
+({} as { ElCard: typeof __VLS_10; }).ElCard;
+({} as { ElCard: typeof __VLS_10; }).ElCard;
+const __VLS_12 = __VLS_11({ ...{}, class: ("box-card"), shadow: ("hover"), style: ({}), }, ...__VLS_functionalComponentArgsRest(__VLS_11));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_10, typeof __VLS_12> & Record<string, unknown>) => void)({ ...{}, class: ("box-card"), shadow: ("hover"), style: ({}), });
+const __VLS_13 = __VLS_pickFunctionalComponentCtx(__VLS_10, __VLS_12)!;
+let __VLS_14!: __VLS_NormalizeEmits<typeof __VLS_13.emit>;
+{
+const __VLS_15 = ({} as 'ElRow' extends keyof typeof __VLS_ctx ? { 'ElRow': typeof __VLS_ctx.ElRow; } : 'elRow' extends keyof typeof __VLS_ctx ? { 'ElRow': typeof __VLS_ctx.elRow; } : 'el-row' extends keyof typeof __VLS_ctx ? { 'ElRow': (typeof __VLS_ctx)["el-row"]; } : typeof __VLS_resolvedLocalAndGlobalComponents).ElRow;
+const __VLS_16 = __VLS_asFunctionalComponent(__VLS_15, new __VLS_15({ ...{}, gutter: ((12)), }));
+({} as { ElRow: typeof __VLS_15; }).ElRow;
+({} as { ElRow: typeof __VLS_15; }).ElRow;
+const __VLS_17 = __VLS_16({ ...{}, gutter: ((12)), }, ...__VLS_functionalComponentArgsRest(__VLS_16));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_15, typeof __VLS_17> & Record<string, unknown>) => void)({ ...{}, gutter: ((12)), });
+const __VLS_18 = __VLS_pickFunctionalComponentCtx(__VLS_15, __VLS_17)!;
+let __VLS_19!: __VLS_NormalizeEmits<typeof __VLS_18.emit>;
+{
+const __VLS_20 = ({} as 'ElCol' extends keyof typeof __VLS_ctx ? { 'ElCol': typeof __VLS_ctx.ElCol; } : 'elCol' extends keyof typeof __VLS_ctx ? { 'ElCol': typeof __VLS_ctx.elCol; } : 'el-col' extends keyof typeof __VLS_ctx ? { 'ElCol': (typeof __VLS_ctx)["el-col"]; } : typeof __VLS_resolvedLocalAndGlobalComponents).ElCol;
+const __VLS_21 = __VLS_asFunctionalComponent(__VLS_20, new __VLS_20({ ...{}, span: ((21)), }));
+({} as { ElCol: typeof __VLS_20; }).ElCol;
+({} as { ElCol: typeof __VLS_20; }).ElCol;
+const __VLS_22 = __VLS_21({ ...{}, span: ((21)), }, ...__VLS_functionalComponentArgsRest(__VLS_21));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_20, typeof __VLS_22> & Record<string, unknown>) => void)({ ...{}, span: ((21)), });
+const __VLS_23 = __VLS_pickFunctionalComponentCtx(__VLS_20, __VLS_22)!;
+let __VLS_24!: __VLS_NormalizeEmits<typeof __VLS_23.emit>;
+(__VLS_ctx.todo in __VLS_ctx.todos);
+(__VLS_23.slots!).default;
+}
+{
+const __VLS_25 = ({} as 'ElCol' extends keyof typeof __VLS_ctx ? { 'ElCol': typeof __VLS_ctx.ElCol; } : 'elCol' extends keyof typeof __VLS_ctx ? { 'ElCol': typeof __VLS_ctx.elCol; } : 'el-col' extends keyof typeof __VLS_ctx ? { 'ElCol': (typeof __VLS_ctx)["el-col"]; } : typeof __VLS_resolvedLocalAndGlobalComponents).ElCol;
+const __VLS_26 = __VLS_asFunctionalComponent(__VLS_25, new __VLS_25({ ...{}, span: ((3)), }));
+({} as { ElCol: typeof __VLS_25; }).ElCol;
+({} as { ElCol: typeof __VLS_25; }).ElCol;
+const __VLS_27 = __VLS_26({ ...{}, span: ((3)), }, ...__VLS_functionalComponentArgsRest(__VLS_26));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_25, typeof __VLS_27> & Record<string, unknown>) => void)({ ...{}, span: ((3)), });
+const __VLS_28 = __VLS_pickFunctionalComponentCtx(__VLS_25, __VLS_27)!;
+let __VLS_29!: __VLS_NormalizeEmits<typeof __VLS_28.emit>;
+{
+const __VLS_30 = ({} as 'ElButton' extends keyof typeof __VLS_ctx ? { 'ElButton': typeof __VLS_ctx.ElButton; } : 'elButton' extends keyof typeof __VLS_ctx ? { 'ElButton': typeof __VLS_ctx.elButton; } : 'el-button' extends keyof typeof __VLS_ctx ? { 'ElButton': (typeof __VLS_ctx)["el-button"]; } : typeof __VLS_resolvedLocalAndGlobalComponents).ElButton;
+const __VLS_31 = __VLS_asFunctionalComponent(__VLS_30, new __VLS_30({ ...{ onClick: {} as any, }, type: ("success"), icon: ("el-icon-check"), circle: (true), }));
+({} as { ElButton: typeof __VLS_30; }).ElButton;
+({} as { ElButton: typeof __VLS_30; }).ElButton;
+const __VLS_32 = __VLS_31({ ...{ onClick: {} as any, }, type: ("success"), icon: ("el-icon-check"), circle: (true), }, ...__VLS_functionalComponentArgsRest(__VLS_31));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_30, typeof __VLS_32> & Record<string, unknown>) => void)({ ...{ onClick: {} as any, }, type: ("success"), icon: ("el-icon-check"), circle: (true), });
+const __VLS_33 = __VLS_pickFunctionalComponentCtx(__VLS_30, __VLS_32)!;
+let __VLS_34!: __VLS_NormalizeEmits<typeof __VLS_33.emit>;
+let __VLS_35 = { 'click': __VLS_pickEvent(__VLS_34['click'], ({} as __VLS_FunctionalComponentProps<typeof __VLS_31, typeof __VLS_32>).onClick) };
+__VLS_35 = {
+click: $event => {
+__VLS_ctx.callremoveTodo();
+// @ts-ignore
+[todo, todos, callremoveTodo,];
+}
+};
+}
+(__VLS_28.slots!).default;
+}
+(__VLS_18.slots!).default;
+}
+(__VLS_13.slots!).default;
+}
+(__VLS_8.slots!).default;
+}
+(__VLS_3.slots!).default;
+}
+if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+}
+var __VLS_slots!: {};
+return __VLS_slots;
+}
